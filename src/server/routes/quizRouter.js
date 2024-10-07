1698d5b0c3bc49ffcc3c698b1399454e59d030d4
Prathamesh Mane <134224065+Prathamesh0901@users.noreply.github.com>
2024-10-07 22:02:55 +0530

    Added quiz routes (#39)
    * added quiz routes
    
    * added quiz routes

---
+++ src/server/routes/quizRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const authenticateUser = require('../middlewares/Auth');
+const {createQuiz, updateQuiz, deleteQuiz, getQuiz} = require('../controllers/quizController');
+
+router.post('/', authenticateUser, createQuiz);
+router.put('/:id', authenticateUser, updateQuiz);
+router.delete('/:id', authenticateUser, deleteQuiz);
+router.get('/:id', getQuiz);
+
+module.exports = router;
