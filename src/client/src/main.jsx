adf87cd589e66cc07deebf7edf8647bdfb769e0e
asmb123 <asmit.barua.phe23@itbhu.ac.in>
2024-10-11 01:08:35 +0530

    feat: Landing page

--- src/client/src/main.jsx
+++ src/client/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
